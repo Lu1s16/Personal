1e19c48084c79f52ef6fd4753a3c1f9cc1b46404
Lu1s16 <3045905330115@ingenieria.usac.edu.gt>
2026-04-28 17:35:16 -0600

    endpoint get users

--- main.js
+++ main.js
@@ -2,9 +2,25 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+lista_users = []
+
+
 app.get("/", (req, res) => {
     res.send("Hello World!")
 })
+
+app.get("/get/users", (req, res) => {
+
+    res.send(lista_users)
+
+})
+
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
