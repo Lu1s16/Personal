dbbc5169d52ec462e24cab52493afaec7321d590
Lu1s16 <3045905330115@ingenieria.usac.edu.gt>
2026-04-28 18:08:04 -0600

    endpoint post

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-lista_users = []
+let lista_users = []
+
+app.use(express.json());
 
 
 app.get("/", (req, res) => {
@@ -12,6 +14,23 @@ app.get("/", (req, res) => {
 app.get("/get/users", (req, res) => {
 
     res.send(lista_users)
+
+})
+
+app.post("/post/users", (req, res) => {
+
+    const ID  = req.body.ID
+    const nombre = req.body.Nombre
+    const edad = req.body.Edad 
+
+
+
+    lista_users.push({"ID": ID, "Nombre": nombre, "Edad": edad})
+
+
+    res.send("Usuario agregado")
+
+
 
 })
 
